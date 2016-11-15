9377f5b2dcd1c9e400c5fad2ce2849fa18e92070
MokaMokiMoke <maxfries@t-online.de>
2016-11-15 11:03:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-sudo apt-get install vim htop iotop nload sysbench pigz rsync git tmux \
+sudo apt-get install vim htop iotop nload sysbench pigz rsync git tmux shellinabox \
 python python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-mysqldb python-memcache python-urllib3 \
 mysql-server nginx memcached
