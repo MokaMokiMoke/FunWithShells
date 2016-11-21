d5dc4efd98f9135360be0c14569809a040e6337b
MokaMokiMoke <maxfries@t-online.de>
2016-11-21 16:08:06 +0100

    Update install.sh
    Added update, upgrade and tree

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-sudo apt-get install vim htop iotop nload sysbench pigz rsync git tmux shellinabox \
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vim htop iotop nload sysbench pigz rsync git tmux shellinabox tree \
 python python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-mysqldb python-memcache python-urllib3 \
 mysql-server nginx memcached
 
 sudo apt-get autoclean
-suto apt-get autoremove
+sudo apt-get autoremove
