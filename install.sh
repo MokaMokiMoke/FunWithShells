3169f0c9c959d6b63a66b541bdff8415b11bfa37
MokaMokiMoke <maxfries@t-online.de>
2016-11-21 16:11:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
-sudo apt-get install vim htop iotop nload sysbench pigz rsync git tmux shellinabox tree \
-python python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-mysqldb python-memcache python-urllib3 \
-mysql-server nginx memcached
+# Basic stuff for a friendly enviroment
+sudo apt-get install -y vim htop iotop nload sysbench pigz rsync git tmux shellinabox tree
 
-sudo apt-get autoclean
-sudo apt-get autoremove
+# Basic stuff for seafile
+sudo apt-get install -y python python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-mysqldb python-memcache python-urllib3
+
+#Stuff for a seafile server instance
+sudo apt-get install -y mysql-server nginx memcached
+
+sudo apt-get -y autoclean
+sudo apt-get -y autoremove
