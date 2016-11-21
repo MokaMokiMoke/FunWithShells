2b9c0bb8bc31de56edffb5c2eb4a9dad2ed5949b
MokaMokiMoke <maxfries@t-online.de>
2016-11-21 16:30:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y vim htop iotop nload sysbench pigz rsync git tmux shelli
 # Basic stuff for seafile
 sudo apt-get install -y python python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-mysqldb python-memcache python-urllib3
 
-#Stuff for a seafile server instance
+# Stuff for a seafile server instance
 sudo apt-get install -y mysql-server nginx memcached
 
 sudo apt-get -y autoclean
