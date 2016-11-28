3866f9aefa274837b79affb154f49e26394ca5fa
MokaMokiMoke <maxfries@t-online.de>
2016-11-28 10:01:14 +0100

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,30 +1,47 @@
-#!/bin/bash                                                                                                                                 
-# Simple benchmark script for sysbench CPU test                                                                                             
-# Usage ./benchmark.sh [maxThreads]                                                                                                         
-                                                                                                                                            
-# Default value for threads                                                                                                                 
-maxThreads=4                                                                                                                                
-maxPrimes=200                                                                                                                               
-                                                                                                                        
-                                                                                                                                            
-# If argument was passed set threads                                                                                                        
-if [ $# -eq 1 ]                                                                                                                             
-        then                                                                                                                                
-        $maxThreads=$1                                                                                                                      
-fi                                                                                                                                          
-                                                                                                                                            
-clock=$(lscpu | grep MHz | cut -d ":" -f2 | cut -d "," -f1)                                                                                                
-echo CPU clock: $clock                                                                                                                      
-echo                                                                                                                                        
-                                                                                                                                            
-for ((numThreads=1; numThreads <= $maxThreads; numThreads=numThreads*2))                                                                    
-        do                                                                                                                                  
-        echo $testRuns test runs with $numThreads threads:                                                                                  
-        for run in {1..3}                                                                                                           
-                do                                                                                                                          
-                time=$(sysbench --test=cpu --cpu-max-prime=$maxPrimes --num-threads=$numThreads run | \                                     
-                grep "execution time" | cut -d ":" -f2 | cut -d "/" -f1)                                                                    
-                echo Run $run with $numThreads threads: $time s                                                                             
-        done                                                                                                                                
-echo                                                                                                                                        
-done  
+#!/bin/bash 
+# Simple benchmark script for sysbench CPU test
+# Usage ./benchmark.sh [maxThreads] [maxPrimes] [numRuns]
+
+# Default value for threads
+maxThreads=8
+maxPrimes=20000
+numRuns=5
+
+# If argument handling
+if [ $# -eq 1 ]; then
+	if [ $1 == "--help" ]; then
+		echo "Usage ./benchmark.sh [maxThreads] [maxPrimes] [numRuns]"
+		exit
+	fi
+	maxThreads=$1
+fi
+
+if [ $# -eq 2 ]; then
+	maxThreads=$1
+	maxPrimes=$2
+fi
+
+if [ $# -eq 3 ]; then
+	maxThreads=$1
+	maxPrimes=$2
+	numRuns=$3
+fi
+
+clock=$(lscpu | grep MHz)
+echo CPU clock:    $clock
+echo Max. Threads: $maxThreads
+echo Max. Primes:  $maxPrimes
+echo No. Runs:     $numRuns 
+echo
+
+for ((numThreads=1; numThreads <= $maxThreads; numThreads*=2))
+	do
+	echo $numThreads threads:
+	for run in {1..3}
+		do
+		time=$(sysbench --test=cpu --cpu-max-prime=$maxPrimes --num-threads=$numThreads run | \
+		grep "execution time" | cut -d ":" -f2 | cut -d "/" -f1)
+		echo Run $run with $numThreads threads: $time s     
+	done
+	echo
+done
