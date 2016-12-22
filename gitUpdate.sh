aa1fb57922aa1dbe1217a42ed3b1233ecaa9736d
MokaMokiMoke <maxfries@t-online.de>
2016-12-22 15:37:53 +0100

    Add files via upload

---
+++ gitUpdate.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/MokaMokiMoke/Seafile
+git clone https://github.com/MokaMokiMoke/BinaryClock
+git clone https://github.com/MokaMokiMoke/FunWithShells
+git clone https://github.com/MokaMokiMoke/HashBase
+git clone https://github.com/MokaMokiMoke/RandomFileGenerator
+git clone https://github.com/MokaMokiMoke/FloydWarshall
+git clone https://github.com/MokaMokiMoke/Weihnachtsraetsel
+git clone https://github.com/MokaMokiMoke/isFullPrimeRecursive
+git clone https://github.com/MokaMokiMoke/BCryptTest
+git clone https://github.com/MokaMokiMoke/BarGraphDrawer
+git clone https://github.com/MokaMokiMoke/NetMonitor
+git clone https://github.com/MokaMokiMoke/BigIntegerCalculator
