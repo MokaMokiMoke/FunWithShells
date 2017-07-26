7cca7a9433d3c0d8c6b2cade3eda70e1dfd2ec58
MokaMokiMoke <maxfries@t-online.de>
2017-07-26 17:29:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # Basic stuff for a friendly enviroment
-sudo apt-get install -y vim htop iotop nload sysbench pigz rsync git tmux shellinabox tree nmap ghostscript curl
+sudo apt-get install -y vim htop iotop nload sysbench pigz rsync git tmux tree nmap ghostscript curl
+
+# Optional
+# sudo apt-get install shellinabox
 
 # Basic stuff for seafile
 # sudo apt-get install -y python python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-mysqldb python-memcache python-urllib3
