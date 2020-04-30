0633ca479fba9aa4099759accc6d0e0b68203e78
Maximilian Fries <mfries@r-m.de>
2020-04-30 10:46:43 +0200

    Added splitXml.sh to split structured xml files line-by-line

---
+++ log-tools/splitXml.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+rm -rf ./bikes
+mkdir bikes
+
+for i in {1..490}; do
+	bike=$(cat alle.xml | head -n 1 | tr -d '\t' | tr -d \$'\r'i)
+	sed -i '1d' alle.xml
+	head -n 28 alle.xml > ./bikes/"$bike"
+	sed -i '1,29d' alle.xml
+done
