6ede8144d8e4a05b1f4871462ed308a2467cf629
MokaMokiMoke <maxfries@t-online.de>
2020-05-05 13:15:02 +0200

    Update splitXml.sh

--- log-tools/splitXml.sh
+++ log-tools/splitXml.sh
@@ -3,7 +3,7 @@ rm -rf ./bikes
 mkdir bikes
 
 for i in {1..490}; do
-	bike=$(cat alle.xml | head -n 1 | tr -d '\t' | tr -d \$'\r'i)
+	bike=$(cat alle.xml | head -n 1 | tr -d '\t' | tr -d \$'\r')
 	sed -i '1d' alle.xml
 	head -n 28 alle.xml > ./bikes/"$bike"
 	sed -i '1,29d' alle.xml
