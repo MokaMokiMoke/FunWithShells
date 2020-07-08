0ba38c0992b564ef0ac117b4b07e2090d023b5b8
MokaMokiMoke <maxfries@t-online.de>
2020-07-08 16:49:47 +0200

    Fixed inconsistency

--- log-tools/xml/preflight.sh
+++ log-tools/xml/preflight.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -rf bikes/ 2> /dev/null
 mkdir bikes
+mkdir backup 2> /dev/null
--- log-tools/xml/splitXml.sh
+++ log-tools/xml/splitXml.sh
@@ -1,25 +1,37 @@
 #/bin/bash
 in="$1"
+start=$(date +"%Y-%m-%d_%H-%M-%S")
+echo Start: $start
 
 # Empty ./bikes folder
 sh ./preflight.sh
+
+# Bakup input xml file
+cp $in ./backup/$start-$in
 
 max=$(cat $in | grep '/ebike' | wc -l)
 
 echo "Expecting: $max bikes"
 
+ctr=1
 for i in $(seq 1 $max); do
 	bike=$(cat $in | head -n 1 | tr -d '\t' | tr -d \$'\r')
 	sed -i '1d' $in
 	head -n 28 $in > ./bikes/"$bike"
 	sed -i '1,29d' $in
+        if [ $(( $ctr % 10 )) -eq 0 ]; then
+            echo $ctr..
+        fi
+        let ctr++
 done
 
 echo "Generated: $(ls ./bikes/*.xml | wc -l) bikes"
-arc="bikes.zip"
+arc="$start-bikes.zip"
 zip -qqr $arc ./bikes/ 
-echo "Bikes compressed: $arc" 
+echo "Compressed: $arc, size: $(du -k "$arc" | cut -f1) kB" 
 
-if ( $(cat $in | wc -l) == 0 ); then
+if [ $(cat $in | wc -l) -eq 0 ]; then
     rm $in
 fi
+
+echo "Ende: $(date +"%Y-%m-%d_%H-%M-%S")"
