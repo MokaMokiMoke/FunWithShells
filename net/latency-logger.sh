ed48f4535bfddf0f31c25ddb603402ca05ba2155
MokaMokiMoke <maxfries@t-online.de>
2020-08-19 12:06:23 +0100

    Added log-path

--- net/latency-logger.sh
+++ net/latency-logger.sh
@@ -3,13 +3,14 @@
 declare -a hosts=("heise.de" "195.50.140.114" "9.9.9.9" "1.1.1.1" "netflix.com" "google.com" "8.8.8.8" "192.168.0.1")
 noOfPings=5
 timeout=20
+out=/mnt/omv/public/latency-log/
 
 while true; do
 	 for host in "${hosts[@]}"; do
-		echo -e `date +"%Y-%m-%d_%H-%M-%S\c"` | tee -a $host.log
-		echo -e ";\c" | tee -a $host.log
+		echo -e `date +"%Y;%m;%d;%H;%M;%S\c"` | tee -a $out$host.log
+		echo -e ";\c" | tee -a $out$host.log
 		ping -c $noOfPings -w $timeout $host | grep time= | rev | cut -d " " -f2 | rev | \
-		tr '\n' ';' | sed 's/time=//g' | tee -a $host.log
-		printf "\n" | tee -a $host.log
+		tr '\n' ';' | sed 's/time=//g' | sed 's/\./,/g' | tee -a $out$host.log
+		printf "\n" | tee -a $out$host.log
 	done
 done
