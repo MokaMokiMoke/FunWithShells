28aaad65ebfd42696c498ba2f8f1d5955038d70f
MokaMokiMoke <maxfries@t-online.de>
2020-08-19 10:26:35 +0100

    Removed time string for logging

--- net/latency-logger.sh
+++ net/latency-logger.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 declare -a hosts=("heise.de" "195.50.140.114" "9.9.9.9" "1.1.1.1" "netflix.com" "google.com" "8.8.8.8" "192.168.0.1")
-noOfPings=10
+noOfPings=5
+timeout=20
 
 while true; do
 	 for host in "${hosts[@]}"; do
 		echo -e `date +"%Y-%m-%d_%H-%M-%S\c"` | tee -a $host.log
 		echo -e ";\c" | tee -a $host.log
-		ping -c 5 $host | grep time= | rev | cut -d " " -f2 | rev | tr '\n' ';' | tee -a $host.log
+		ping -c $noOfPings -w $timeout $host | grep time= | rev | cut -d " " -f2 | rev | \
+		tr '\n' ';' | sed 's/time=//g' | tee -a $host.log
 		printf "\n" | tee -a $host.log
 	done
 done
