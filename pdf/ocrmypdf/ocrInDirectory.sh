6ab17ce5193dc99cabfb9db8e98e7cc96cd29f8b
MokaMokiMoke <maxfries@t-online.de>
2020-08-28 14:29:10 +0200

    Added verbose mode, added dependency checkup

--- pdf/ocrmypdf/ocrInDirectory.sh
+++ pdf/ocrmypdf/ocrInDirectory.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-for i in *.pdf; do
-        echo "START: $i"
+# Check if dependencies are installed
 
+verbose=">/dev/null 2>&1"
+if [ $1 = "--verbose" ]; then
+	verbose=""
+fi
+
+declare -a dep=("ocrmypdf" "tesseract" "ghostscript")
+for d in "${dep[@]}"; do
+	if ! command -v $d &> /dev/null; then
+		echo "$d could not be found!"
+		exit
+	fi
+done
+
+lang="deu"
+if [ -z $(tesseract --list-langs | grep $lang) ]; then
+	echo "tesseract language $lang could not be found!"
+	exit
+fi
+
+
+find . -type f -iname "*.pdf" | while read i; do
+        echo "START: $i"
         oHash=$(md5sum "$i" | awk '{ print $1 }')
         sTime=$(date +%s)
         oSize=$(wc -c "$i" | cut -d ' ' -f1)
 
-        #ocrmypdf -l deu --optimize 2 --redo-ocr "$i" "$(basename "$i" ".pdf")_ocr.pdf" >/dev/null 2>&1
-        ocrmypdf -l deu --optimize 2 --redo-ocr "$i" "$(basename "$i" ".pdf").pdf" >/dev/null 2>&1
+        ocrmypdf -l deu --optimize 2 --redo-ocr --output-type pdf "$i" "$(basename "$i" ".pdf").pdf" $verbose
 
         nHash=$(md5sum "$i" | awk '{ print $1 }')
         eTime=$(date +%s)
@@ -18,6 +38,6 @@ for i in *.pdf; do
         else
                 echo -e "\tSometing changed"
         fi
-        echo "ENDE: $i, Time: $((eTime-sTime)) [s], -$((oSize-nSize)) [Byte]"
+        echo "ENDE: Time: $((eTime-sTime)) [s], -$((oSize-nSize)) [Byte]"
         echo ""
 done
