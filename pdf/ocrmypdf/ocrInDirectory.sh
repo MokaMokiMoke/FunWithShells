b6cd8b6d9a6d22d914c1387d0c401125530a3c4b
Maximilian Fries <mfries@r-m.de>
2020-09-10 14:01:58 +0200

    Fixed quiet/verobse mode

--- pdf/ocrmypdf/ocrInDirectory.sh
+++ pdf/ocrmypdf/ocrInDirectory.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # Check if dependencies are installed
 
-verbose=">/dev/null 2>&1"
-if [ $1 = "--verbose" ]; then
-	verbose=""
+if [ $# -gt 1 ]; then
+	echo Invalid number of parameters
+	echo Usage: ocrmypdf [--quiet]
+	exit 1
 fi
+
+quiet=""
+if [ $# -gt 0 ] && [ $1 = "--quiet" ]; then
+	quiet="--quiet"
+fi
+
+# DEBUG
+# echo quiet: $quiet, hash: $#, 1st: $1
 
 declare -a dep=("ocrmypdf" "tesseract" "ghostscript")
 for d in "${dep[@]}"; do
@@ -27,7 +36,7 @@ find . -type f -iname "*.pdf" | while read i; do
         sTime=$(date +%s)
         oSize=$(wc -c "$i" | cut -d ' ' -f1)
 
-        ocrmypdf -l deu --optimize 2 --redo-ocr --output-type pdf "$i" "$(basename "$i" ".pdf").pdf" $verbose
+        ocrmypdf -l deu --optimize 2 --redo-ocr --output-type pdf "$i" "$(basename "$i" ".pdf").pdf" $quiet
 
         nHash=$(md5sum "$i" | awk '{ print $1 }')
         eTime=$(date +%s)
